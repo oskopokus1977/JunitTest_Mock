433690e82dc1575c145736994c15f5592948f845
oskopokus1977 <oskopokus1977@gmail.com>
2017-11-13 01:03:37 +0200

    Parameterized.Parameters

--- src/test/java/com/ParamCalculatorTest.java
+++ src/test/java/com/ParamCalculatorTest.java
@@ -9,21 +9,24 @@ import org.junit.runners.Parameterized;
 public class ParamCalculatorTest {
 
     @Parameterized.Parameter(0)
-     int number1;
-    @Parameterized.Parameter(1)
-     int number2;
-    @Parameterized.Parameter(2)
-     int result;
+    public int number1;
 
+    @Parameterized.Parameter(1)
+    public int number2;
+
+    @Parameterized.Parameter(2)
+    public int number3;
+
+    @Parameterized.Parameters
     public static Object[][] getData() {
-        return new Object[][]{{1, 1, 2}, {34, 26, 60}, {7, 5, 13}};
+        return new Object[][]{{1, 1, 2}, {34, 26, 60}, {7, 5, 12}};
     }
 
     @Test
     public void addMethodTest(){
         Calculator calculator = new Calculator();
         int sum = calculator.add(number1,number2);
-        Assert.assertSame(sum, result);
+        Assert.assertSame(sum, number3);
     }
 
 }
